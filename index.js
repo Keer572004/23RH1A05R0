2576c27ab2b39808f87291c6e795be00515ee745
Keer572004 <akhilkirthu@gmail.com>
2025-07-03 16:14:26 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,6 @@
+const mod=require('./module');
+
+console.log(mod.pi);
+console.log(mod.add(10,5));
+console.log(mod.add(10,mod.pi));
+console.log(mod.sub(10,5));
---
+++ module.js
@@ -0,0 +1,6 @@
+const pi=3.14;
+const add=(a,b)=>(a+b);
+const sub=(a,b)=>(a-b);
+module.exports.pi=pi;
+module.exports.add=add;
+module.exports.sub=sub;
